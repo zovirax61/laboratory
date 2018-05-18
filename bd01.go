5c3ecc794b77dbc1ad09af7f0bcfe0bbc80899c1
zovirax61 <bertrand.dafflon@gmail.com>
2018-05-18 10:51:47 +0200

    création du pgm bd01.go

---
+++ bd01.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const rsc string = "* * * Rossens System Center * * *"
+
+func main() {
+
+    platform := "plasticlimit welcomes you"
+    fmt.Println(rsc)
+    fmt.Println(platform)
+
+    fmt.Println("hello strange creature" + " that appears to have stepped out of a legend")
+
+	i := 1
+    	for i <= 3 {
+        	fmt.Println(i)
+        	i = i + 1
+    	}
+
+	for j := 7; j <= 9; j++ {
+    	    fmt.Println(j)
+    	}
+
+    var chiffre int = 8
+
+	if chiffre%4 == 0 {
+    	    fmt.Println(chiffre,"is divisible by 4")
+    	} else {
+    	    fmt.Println(chiffre,"isn't divisible by 4")
+    	} 	
+
+   	
+}
